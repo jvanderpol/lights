5f3a8f5684ca70b5168f7168d5aa696ff3e37a14
John VanderPol <john.vanderpol@gmail.com>
2017-06-17 17:22:32 -0700

    Changed to a command that will not fail

--- web/action.php
+++ web/action.php
@@ -3,7 +3,13 @@ header('Content-type: application/json');
 
 $action = $_REQUEST["action"];
 $device_id = $_REQUEST["device_id"];
-$command = "light_command " . $action . ' ' . $device_id;
+
+# Using a useless command now since it won't return an error
+$command = "echo foo";
+
+# This would be something closer to the real thing
+# $command = "light_command " . $action . ' ' . $device_id;
+
 $result;
 $result_code;
 exec($command . " 2>&1", $result, $result_code);
